07324998adbfcb976604175c74d818800e346dc7
Md Ikhtiar Uddin <mdikhtiaruddin786@gmail.com>
2023-11-10 20:06:56 +0600

    sorting

---
+++ Sorting/insertion_Sort.py
@@ -0,0 +1,15 @@
+# insertion sort , quick sort , merge sort, selection sort
+
+def insertionSort(array):
+
+    for i in range(1, len(array)):
+        key = array[i]
+        j = i - 1        
+        while j >= 0 and key < array[j]:
+            array[j + 1] = array[j]
+            j = j - 1
+        array[j + 1] = key
+data = [9, 5, 1, 4, 3]
+insertionSort(data)
+print('Sorted Array in Ascending Order:')
+print(data)
